90efc09b2db8c1bee4900248accc2783a6ae57c2
angrytoro <angrytoro@gmail.com>
2014-09-17 11:10:14 +0800

    finished most of function

--- ngscrollbar.js
+++ ngscrollbar.js
@@ -6,59 +6,102 @@
 
 // todo 先处理垂直方向
 angular.module('widget.scrollbar', [])
-.directive('ngScrollbar', ['$timeout',function($timeout) {
+.directive('ngScrollbar', ['$timeout',
+	function($timeout) {
+		return {
+			restrict: 'AE',
+			transclude: true,
+			scope: {
+				scrollbarConfig: '=scrollbarConfig',
+				scrollbarX: '@',
+				scrollbarY: '@'
+			},
+			template: '<div style="position:relative;width:100%;height:100%;">\
+							<div class="ngscroll-content-container" style="position:absolute;top:0;left:0;transition: top .4s linear, left .4s linear;" ng-transclude>\
+							</div>\
+					   		<ng-scrollbar-x ng-if="scrollbarX || scrollbarX === undefined"></ng-scrollbar-x>\
+					   		<ng-scrollbar-y ng-if="scrollbarY || scrollbarY === undefined"></ng-scrollbar-y>\
+					   </div>',
+			controller: 'scrollbarController',
+			compile: function(element, attrs) {
+				element.css('overflow', 'hidden');
+				console.log('ngScrollbar compile');
+				return function(scope, element, attrs, ctrl) {
+					console.log('ngScrollbar link');
+					var defaultConfig = {
+						dragSpeed: 1, //default browser delta value is 120 or -120
+						firefoxDragSpeed: 40, // the ff delta value is 3 or -120 when scroll
+						scrollbar: {
+							width: 6,
+							hoverWidth: 8,
+							color: 'rgba(0,0,0,.6)'
+						},
+						scrollbarContainer: {
+							width: 12,
+							color: 'rgba(0,0,0,.1)'
+						}
+					};
+
+					var config = angular.copy(angular.extend(scope.scrollbarConfig || {}, defaultConfig)), //if don't use copy the config will be the default value of scope.scrollbarConfig
+						contentElement = angular.element(element[0].querySelector('.ngscroll-content-container')),
+						contentDom = contentElement[0],
+						scrollbarMargin = (config.scrollbarContainer.width - config.scrollbar.width) / 2,
+						scrollbarHoverMargin = (config.scrollbarContainer.width - config.scrollbar.hoverWidth) / 2;
+
+					angular.extend(ctrl, {
+						getContainerElement: function() {
+							return element;
+						},
+						getContentElement: function() {
+							return contentElement;
+						},
+						getConfig: function() {
+							return config;
+						},
+						getScrollbarMargin: function() {
+							return scrollbarMargin;
+						},
+						getScrollbarHoverMargin: function() {
+							return scrollbarHoverMargin;
+						}
+					});
+				};
+			}
+		};
+	}
+])
+.controller('scrollbarController', ['$scope', function($scope) {
+	console.log('scrollbarController');
+	// console.log($scope);
+}])
+.directive('ngScrollbarY', ['$timeout', function($timeout){
 	return {
 		restrict: 'AE',
-		transclude: true,
-		scope: {
-			repeatItems: '=repeatItems', // the attribute is used to watch if the content is changed.
-			scrollbarConfig: '@'
-		},
-		template: '<div style="position:relative;width:100%;height:100%;">\
-					<div class="ngscroll-content-container" style="position:absolute;width:100%;top:0;transition: top .4s linear;" ng-transclude>\
-					</div>\
-			   		<div class="ngscrollbar-container" ng-style="styles.scrollbarContainer"><div class="ngscrollbar" ng-style="styles.scrollbar"></div>\
-			   		</div>\
-			   </div>',
-		controller: 'scrollbarController',
+		require: '^ngScrollbar',
+		replace: true,
+		template: '<div class="ngscrollbar-container-y" ng-style="styles.scrollbarContainer"><div class="ngscrollbar-y" ng-style="styles.scrollbar"></div></div>',
 		compile: function(element, attrs) {
-			element.css('overflow', 'hidden');
-			return function($scope, element, attrs, ctrl) {
-				var config = {
-					autoResize: true,
-					direction: 'vertical',
-					dragSpeed: 1, //default browser delta value is 120 or -120
-					firefoxDragSpeed: 40, // the ff delta value is 3 or -120 when scroll
-					scrollTo: null,
-					scrollbar: {
-						width: 6,
-						hoverWidth: 8,
-						color: 'rgba(0,0,0,.6)'
-					},
-					scrollbarContainer: {
-						width: 12,
-						color: 'rgba(0,0,0,.1)'
-					}
-				};
+			console.log('ngScrollbarY compile');
+			return function(scope, element, attrs, ctrl) {
+				console.log('ngScrollbarY link');
 
-				angular.extend(config, $scope.scrollbarConfig || {});
-
-				var docEl = angular.element(document);
-					elDom = element[0];
-					contentContainer = angular.element(elDom.querySelector('.ngscroll-content-container')), //the container of content
-					scrollbarContainer = angular.element(elDom.querySelector('.ngscrollbar-container')),
-					scrollbar = angular.element(elDom.querySelector('.ngscrollbar')),
-					scrollbarMargin = (config.scrollbarContainer.width - config.scrollbar.width) / 2,
-					scrollbarHoverMargin = (config.scrollbarContainer.width - config.scrollbar.hoverWidth) / 2;
+				var config = ctrl.getConfig(),
+					docEl = angular.element(document),
+					containerElement = ctrl.getContainerElement(),
+					containerDom = containerElement[0],
+					contentElement = ctrl.getContentElement(), //the container of content
+					scrollbarContainer = angular.element(containerDom.querySelector('.ngscrollbar-container-y')),
+					scrollbar = angular.element(containerDom.querySelector('.ngscrollbar-y')),
+					scrollbarMargin = ctrl.getScrollbarMargin(),
+					scrollbarHoverMargin = ctrl.getScrollbarHoverMargin();
 					// containerHeight = element[0].offsetHeight, // the element which need to mock scrollbar;
 					// containerMaxHeight = element[0].style.maxHeight;
 
-				$scope.styles = {
+				scope.styles = {
 					scrollbarContainer: {
 						position: 'absolute',
 						height: '100%',
 						transition: 'background .3s ease-in-out',
-						// background: config.scrollbarContainer.color,
 						'border-radius': config.scrollbarContainer.width / 2 + 'px'
 					},
 					scrollbar: {
@@ -78,22 +121,16 @@ angular.module('widget.scrollbar', [])
 				scrollbar.css('right', scrollbarMargin + 'px'); //set scrollbar right
 				scrollbar.css('width', config.scrollbar.width + 'px');
 
-				if ($scope.repeatItems) {
-					$scope.$watch('repeatItems', function() { // when the content is changed.
-						$timeout(reset, 5);
-					});
-				}
-
 				var getContentHeight = function() {
-					return contentContainer[0].offsetHeight;
+					return contentElement[0].offsetHeight;
 				};
 
 				var getContainerHeight = function() {
-					return element[0].offsetHeight;
+					return containerElement[0].offsetHeight;
 				};
 
 				var getScrollbarHeight = function() {
-					var height = Math.pow(getContainerHeight(), 2) / contentContainer[0].offsetHeight - scrollbarMargin*2;
+					var height = Math.pow(getContainerHeight(), 2) / contentElement[0].offsetHeight - scrollbarMargin*2;
 					return height;
 				};
 
@@ -122,16 +159,16 @@ angular.module('widget.scrollbar', [])
 
 				var scrollTo = function(top) {
 					top = Math.min(0, Math.max(top, getContainerHeight() - getContentHeight()));
-					contentContainer.css('top', top + 'px');
+					contentElement.css('top', top + 'px');
 					scrollbar.css('top', -top/getContentHeight()*getContainerHeight() + scrollbarMargin + 'px');
 				};
 
 				var scroll = function(distance) {
-					var newTop = parseInt(contentContainer.css('top'), 10) + distance;
+					var newTop = parseInt(contentElement.css('top'), 10) + distance;
 					scrollTo(newTop);
 				};
 
-				element.on('mousewheel', function(event) {
+				containerElement.on('mousewheel', function(event) {
 					if (!isOverflow()) {
 						return;
 					}
@@ -143,7 +180,7 @@ angular.module('widget.scrollbar', [])
 				});
 
 				if(window.navigator.userAgent.toLowerCase().indexOf('firefox') >= 0) {
-					element.on('wheel', function(event) {
+					containerElement.on('wheel', function(event) {
 						if (!isOverflow()) {
 							return;
 						}
@@ -169,43 +206,189 @@ angular.module('widget.scrollbar', [])
 					scrollbar.css('border-radius', config.scrollbar.width / 2 + 'px');
 				});
 
-				element.on('mouseenter', function(event) {
+				var scrollbarMousedown = false,
+					axisY,
+					mouseInElement = false;
+
+				containerElement.on('mouseenter', function(event) {
+					mouseInElement = true;
 					showScrollbar();
 				});
-				element.on('mouseleave', function(event) {
+				containerElement.on('mouseleave', function(event) {
+					mouseInElement = false;
 					if (scrollbarMousedown) {
 						return;
 					}
 					hideScrollbar();
 				});
 
-				var scrollbarMousedown = false,
-					coordY;
 				scrollbar.on('mousedown', function(event) {
 					event.preventDefault();
 					scrollbarMousedown = true;
-					coordY = event.screenY;
+					axisY = event.screenY;
 					docEl.one('mouseup', function(event) {
 						scrollbarMousedown = false;
-						hideScrollbar();
+						if (!mouseInElement) {
+							hideScrollbar();
+						}
 						// docEl.off('mouseup', arguments.callee);
 					});
 				});
 				docEl.on('mousemove', function(event) {
 					if(scrollbarMousedown) {
 						event.preventDefault();
-						scroll(-(event.screenY - coordY) * config.dragSpeed * getContentHeight() / getContainerHeight());
-						coordY = event.screenY;
+						scroll(-(event.screenY - axisY) * config.dragSpeed * getContentHeight() / getContainerHeight());
+						axisY = event.screenY;
+					}
+				});
+
+				$timeout(reset,5);
+			}
+		}
+	};
+}])
+.directive('ngScrollbarX', ['$timeout', function($timeout) {
+	return {
+		restrict: 'AE',
+		replace: true,
+		require: '^ngScrollbar',
+		template: '<div class="ngscrollbar-container-x" ng-style="styles.scrollbarContainer"><div class="ngscrollbar-x" ng-style="styles.scrollbar"></div></div>',
+		compile: function(element, attrs) {
+			return function(scope, element, attrs, ctrl) {
+
+				var config = ctrl.getConfig(),
+					docEl = angular.element(document),
+					containerElement = ctrl.getContainerElement(),
+					containerDom = containerElement[0],
+					contentElement = ctrl.getContentElement(), //the container of content
+					// scrollbarContainer = angular.element(containerDom.querySelector('.ngscrollbar-container-x')),
+					scrollbar = angular.element(element[0].querySelector('.ngscrollbar-x')),
+					scrollbarMargin = ctrl.getScrollbarMargin(),
+					scrollbarHoverMargin = ctrl.getScrollbarHoverMargin();
+					// containerHeight = element[0].offsetHeight, // the element which need to mock scrollbar;
+					// containerMaxHeight = element[0].style.maxHeight;
+
+				scope.styles = {
+					scrollbarContainer: {
+						position: 'absolute',
+						width: '100%',
+						transition: 'background .3s ease-in-out',
+						'border-radius': config.scrollbarContainer.width / 2 + 'px'
+					},
+					scrollbar: {
+						position: 'absolute',
+						cursor: 'default',
+						opacity: 0,
+						transition: 'opacity .3s ease-in-out, border-radius .1s linear, width .1s linear, right .1s linear',
+						background: config.scrollbar.color,
+						'border-radius': config.scrollbar.width / 2 + 'px'
+					}
+				};
+
+				element.css('height', config.scrollbarContainer.width + 'px'); // set the scrollbarContainer height;
+				// scrollbarContainer.css('height', '100%');
+				element.css('bottom', 0); // set scrollbarContainer top
+				element.css('left', 0); //set scrollbarContainer left
+				scrollbar.css('top', scrollbarMargin + 'px'); //set scrollbar top
+				scrollbar.css('height', config.scrollbar.width + 'px');
+
+				var getContentWidth = function() {
+					return contentElement[0].offsetWidth;
+				};
+
+				var getContainerWidth = function() {
+					return containerDom.offsetWidth;
+				};
+
+				var getScrollbarWidth = function() {
+					return Math.pow(getContainerWidth(), 2) / getContentWidth() - scrollbarMargin * 2;
+				};
+
+				var showScrollbar = function() {
+					scrollbar.css('opacity', 1);
+				};
+
+				var hideScrollbar = function() {
+					scrollbar.css('opacity', 0);
+				};
+
+				var isOverflow = function() {
+					return getContentWidth() > getContainerWidth();
+				};
+
+				var reset = function() {
+					if (isOverflow()) {
+						element.css('display', 'block');
+						scrollbar.css('left', scrollbarMargin + 'px');
+						scrollbar.css('width', getScrollbarWidth() + 'px');
+					} else {
+						element.css('display', 'none');
+					}
+				};
+
+				var scrollTo = function(left) {
+					left = Math.min(0, Math.max(left, getContainerWidth() - getContentWidth()));
+					contentElement.css('left', left + 'px');
+					scrollbar.css('left', -left/getContentWidth()*getContainerWidth() + scrollbarMargin + 'px');
+				};
+
+				var scroll = function(distance) {
+					var left = parseInt(contentElement.css('left'), 10) + distance;
+					scrollTo(left);
+				};
+
+				element.on('mouseenter', function(event) {
+					element.css('background', config.scrollbarContainer.color);
+					scrollbar.css('height', config.scrollbar.hoverWidth + 'px');
+					scrollbar.css('top', scrollbarHoverMargin + 'px');
+					scrollbar.css('border-radius', config.scrollbar.hoverWidth / 2 + 'px');
+				});
+
+				element.on('mouseleave', function(event) {
+					element.css('background', 'none');
+					scrollbar.css('height', config.scrollbar.width + 'px');
+					scrollbar.css('top', scrollbarMargin + 'px');
+					scrollbar.css('border-radius', config.scrollbar.width / 2 + 'px');
+				});
+
+				var scrollbarMousedown = false,
+					axisX,
+					mouseInElement = false;
+
+				containerElement.on('mouseenter', function(event) {
+					mouseInElement = true;
+					showScrollbar();
+				});
+				containerElement.on('mouseleave', function(event) {
+					mouseInElement = false;
+					if (scrollbarMousedown) {
+						return;
+					}
+					hideScrollbar();
+				});
+
+				scrollbar.on('mousedown', function(event) {
+					event.preventDefault();
+					scrollbarMousedown = true;
+					axisX = event.screenX;
+					docEl.one('mouseup', function(event) {
+						scrollbarMousedown = false;
+						if (!mouseInElement) {
+							hideScrollbar();
+						}
+						// docEl.off('mouseup', arguments.callee);
+					});
+				});
+				docEl.on('mousemove', function(event) {
+					if(scrollbarMousedown) {
+						event.preventDefault();
+						scroll(-(event.screenX - axisX) * config.dragSpeed * getContentWidth() / getContainerWidth());
+						axisX = event.screenX;
 					}
 				});
 
 				$timeout(reset,5);
 			};
 		}
-	};
-}])
-.controller('scrollbarController', ['$scope',
-	function($scope) {
-		console.log($scope);
 	}
-]);
+}]);
